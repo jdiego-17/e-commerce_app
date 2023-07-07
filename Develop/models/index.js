563659c13a6308637081fdfccac5511c0b486b23
Dij17 <121464770+Dij17@users.noreply.github.com>
2023-07-06 22:05:18 -0500

    added the relationship between the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+  Product.belongsTo(Category, {
+    foreignKey: 'tag_name',
+    
+  })
 // Categories have many Products
-
+  Category.hasMany(Product, {
+    foreignKey: 'catergory_id',
+    onDelete: 'CASCADE'
+  })
 // Products belongToMany Tags (through ProductTag)
-
+  Product.belongsTo(ProductTag, {
+    foreignKey: 'tag_id',
+  })
 // Tags belongToMany Products (through ProductTag)
-
+  Tag.belongsTo(ProductTag, {
+    foreignKey: 'category_id'
+  })
 module.exports = {
   Product,
   Category,
